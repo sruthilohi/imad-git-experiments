02449cd52673809a7922b20987173dfeeb56ef6e
anaghdeepak <sruthilohi@gmail.com>
2018-05-15 18:25:34 +0400

    compleated work with new feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function aBigNewFeature (){
 	console.log('added a lot of new code');
+	console.log('completely done with the new feature');
 };
 function something(){
 	console.log('did some thing for testing');
