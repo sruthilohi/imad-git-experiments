847f55736def03e3c91c22a46b7a9a897806dbba
anaghdeepak <sruthilohi@gmail.com>
2018-05-15 18:05:19 +0400

    lf way done with changes

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+function aBigNewFeature (){
+	console.log('added a lot of new code');
+};
 function something(){
 	console.log('did some thing for testing');
 }
 console.log('hello world');
 something();
+aBigNewFeature();
 
+	
